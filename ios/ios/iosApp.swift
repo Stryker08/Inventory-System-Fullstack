e67e87735443fc0693caf54f8fd632113e5f53ba
Stryker <stryker.shaw@fhnw.edu>
2026-04-28 08:48:33 -0600

    chore: added docker mysql and phpmyadmin

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Student on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
